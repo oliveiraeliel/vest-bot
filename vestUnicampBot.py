8d6a9d7a7d13b93d5edb7910bb67735e9ec6e8bf
Eliel Oliveira da Silva <eliel.oliveira2004@gmail.com>
2021-05-25 18:49:33 -0300

    Atualização

--- vestUnicampBot.py
+++ vestUnicampBot.py
@@ -29,6 +29,7 @@ while x==0 :#rotina para o codigo atualizar a cada 15 segundos
 
     horaAgora = datetime.now()#pega o horario atual
     horario = horaAgora.strftime('%H:%M')
+    print(horario)
     if(horario == "18:48"):#verifica se o horario atual é 12:00
         api.update_status("Faltam " + primeiraFaseDias + " dias para a primeira fase e " + segundaFaseDias + " dias para a segunda fase do Vestibular da Unicamp") #Tweeta
         print("Faltam " + primeiraFaseDias + " dias para a primeira fase e " + segundaFaseDias + " dias para a segunda fase do Vestibular da Unicamp")
