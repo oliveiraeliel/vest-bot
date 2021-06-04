3bac70db319679b5667dcadd4b2945410a81b74b
Eliel Oliveira da Silva <eliel.oliveira2004@gmail.com>
2021-06-04 12:00:29 -0300

    Teste

--- vestUnicampBot.py
+++ vestUnicampBot.py
@@ -32,14 +32,15 @@ while True:#rotina para o codigo atualizar a cada 15 segundos
 
     horaAgora = datetime.now()#pega o horario atual
     horario = horaAgora.strftime('%H:%M')
-    print(horario+" "+primeiraFaseDias+" "+segundaFaseDias)
+    print(horario+" "+primeiraFaseDias+" "+segundaFaseDias + " " + enemDias)
     # verifica se o horario atual é 11:00. Como o fuso horario do servidor é 3h adiantado, coloquei 14:00 pro bot twittar ao meio dia
-    if(horario == "14:59"):
+    if(horario == "15:03"):
         #api.update_status("Faltam " + enemDias + " dias para o enem, " + primeiraFaseDias + " dias para a primeira fase e " + segundaFaseDias + "  dias para a segunda fase do Vestibular da Unicamp") #Tweeta
         print("Faltam " + enemDias + " dias para o enem, "+ primeiraFaseDias + " dias para a primeira fase e " + segundaFaseDias + "  dias para a segunda fase do Vestibular da Unicamp")
         print("Deu certo")
-        while horario == "14:59":
+        while horario == "14:03":
             print("Já tweetou")
+            time.sleep(15)
     else:
         print("Não deu a hora ainda...")
     print("Bot running...")
