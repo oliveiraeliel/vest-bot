ac7fe64e22713b3428e00e62cf9d016278928831
Eliel Oliveira da Silva <eliel.oliveira2004@gmail.com>
2021-06-05 12:09:59 -0300

    Tratando erros

--- vestUnicampBot.py
+++ vestUnicampBot.py
@@ -35,13 +35,12 @@ while True:#rotina para o codigo atualizar a cada 15 segundos
     print(horario+" "+primeiraFaseDias+" "+segundaFaseDias + " " + enemDias)
     # verifica se o horario atual é 11:00. Como o fuso horario do servidor é 3h adiantado, coloquei 14:00 pro bot twittar ao meio dia
 
-    if(horario == "15:07"):
-        try:
+    if(horario == "14:00"):
+        try:#não deixa a aplicação morrer quando acontecer erros no servidor
             api.update_status("Faltam " + enemDias + " dias para o enem, " + primeiraFaseDias + " dias para a primeira fase e " + segundaFaseDias + "  dias para a segunda fase do Vestibular da Unicamp") #Tweeta
             print("Deu certo")
         except:
             print("Esse tweet ja existe :c")
-
     else:
         print("Não deu a hora ainda...")
     print("Bot running...")
